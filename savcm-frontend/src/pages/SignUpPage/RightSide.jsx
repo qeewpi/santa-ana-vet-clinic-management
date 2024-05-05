cdc58c2a209ec2820fc681a2cf8d913749918f56
qeewpi <leyashml@gmail.com>
2024-05-06 01:59:45 +0800

    chore: Update SignUpPage layout for responsive design

--- savcm-frontend/src/pages/SignUpPage/RightSide.jsx
+++ savcm-frontend/src/pages/SignUpPage/RightSide.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function RightSide() {
-  return <div className="bg-teal-100 min-h-screen"></div>;
+  return <div className="min-h-screen"></div>;
 }
 
 export default RightSide;
